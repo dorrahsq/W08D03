0a7d450590b5238d2945c5764da05bf0187d3f85
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-30 14:15:29 +0300

    get todo

--- routers/controllers/task.js
+++ routers/controllers/task.js
@@ -11,16 +11,51 @@ const getAllTasks = (req, res) => {
     .equals(user)
     .exec(function (err, todos) {
       if (!todos) {
-        return res.json("you dont have an account");
+        return res.status(404).json("you dont have an account");
       }
       if (!todos.length) {
-        return res.json("you dont have any task");
+        return res.json("you dont have any task"); //status 40.. ??
       }
       if (err) return handleError(err);
       res.json(todos);
     });
 };
 
+//get all deleted todos for one user
+const getAllDeletedTasks = (req, res) => {
+  const { user } = req.body;
+  taskModel
+    .find({ isDeleted: true })
+    .populate("user")
+    .where("user")
+    .equals(user)
+    .exec(function (err, todos) {
+      if (err) return handleError(err);
+      res.json(todos);
+    });
+};
+
+//get one task
+const getTask = (req, res) => {
+  const { _id } = req.body; //task id
+  taskModel
+    .findById({ _id })
+    .then((result) => {
+      if (result.isDeleted == true) {
+        return res.json("this task is deleted");
+      }
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
+
+
 module.exports = {
   getAllTasks,
+  getAllDeletedTasks,
+  getTask,
+
 };
--- routers/routs/task.js
+++ routers/routs/task.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const taskRouter = express.Router();
 
-const { getAllTasks } = require("./../controllers/task");
+const {
+  getAllTasks,
+  getAllDeletedTasks,
+  getTask,
+} = require("./../controllers/task");
 
 taskRouter.get("/", getAllTasks); //not deleted (id for user)
+taskRouter.get("/deltedTasks", getAllDeletedTasks); // deleted
+taskRouter.get("/one", getTask); // by id
 
 module.exports = taskRouter;
