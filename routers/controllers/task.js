1929993f749b2cc58de23ac23650fb6f8449d730
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-30 14:59:30 +0300

    update task

--- routers/controllers/task.js
+++ routers/controllers/task.js
@@ -51,11 +51,56 @@ const getTask = (req, res) => {
     });
 };
 
+//create new Task by user
+const createTask = (req, res) => {
+  const { name, user } = req.body;
+  const newTask = new taskModel({
+    name,
+    user,
+  });
+
+  newTask
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+//update Task
+const updateTask = (req, res) => {
+  const { _id, newName } = req.body;
+
+  taskModel.findById({ _id }).then((result) => {
+    console.log(result);
+    if (result.isDeleted == true) {
+      return res.json(
+        "you cant update on this task because its have been deleted"
+      );
+    } else {
+      taskModel.updateOne({ _id }, { $set: { name: newName } }, function (err) {
+        if (err) return handleError(err);
+      });
+      taskModel
+        .find({ _id })
+        .then((result) => {
+          res.json(result);
+        })
+        .catch((err) => {
+          res.send(err);
+        });
+    }
+  });
+};
 
 
 module.exports = {
   getAllTasks,
   getAllDeletedTasks,
   getTask,
+  createTask,
+  updateTask,
 
 };
--- routers/routs/task.js
+++ routers/routs/task.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const taskRouter = express.Router();
 
-const {
-  getAllTasks,
-  getAllDeletedTasks,
-  getTask,
-} = require("./../controllers/task");
+const { getAllTasks, getAllDeletedTasks , getTask , createTask , updateTask  } = require("./../controllers/task");
 
 taskRouter.get("/", getAllTasks); //not deleted (id for user)
 taskRouter.get("/deltedTasks", getAllDeletedTasks); // deleted
 taskRouter.get("/one", getTask); // by id
+taskRouter.post("/create", createTask);
+taskRouter.put("/update", updateTask);
+
 
 module.exports = taskRouter;
